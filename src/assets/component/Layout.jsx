c42ecade3aa01ecc1e000abb8320496829736a8e
Negar <negarsefatdoost@gmail.com>
2025-10-14 23:54:26 +0330

    fix the bug about children in layout and router

--- src/assets/component/Layout.jsx
+++ src/assets/component/Layout.jsx
@@ -2,7 +2,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
 
-export default function Layout({ children }) {
+export default function Layout() {
   return (
     <div
       style={{
